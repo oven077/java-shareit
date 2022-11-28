400e3ac7c70bc7a43fa992861df003c26613357a
Andres <oven077@gmail.com>
2022-11-28 23:02:00 +0300

    sprint 13 (final 2) CodeStyle 2 + users

--- src/main/java/ru/practicum/shareit/item/dao/ItemDao.java
+++ src/main/java/ru/practicum/shareit/item/dao/ItemDao.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.item.dao;
 import ru.practicum.shareit.dao.Dao;
 import ru.practicum.shareit.item.model.Item;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/ru/practicum/shareit/user/dao/UserDao.java
+++ src/main/java/ru/practicum/shareit/user/dao/UserDao.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.user.dao;
 import ru.practicum.shareit.dao.Dao;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
