8162d406a804ddab7bdebe6a1cf0958250a59222
Andres <oven077@gmail.com>
2022-11-29 23:12:49 +0300

    sprint 13 (final 3) + ResponseEntity

--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -19,6 +19,7 @@ import java.util.Collection;
 @RequestMapping(path = "/users")
 public class UserController {
     private final UserService userService;
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
