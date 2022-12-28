bf4ed6c88ca650739b479f84f24294423ba08b3c
Andres <oven077@gmail.com>
2022-12-28 16:06:14 +0300

    check all test and restore properties to H2 4

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -6,7 +6,6 @@ import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
-import java.io.Serializable;
 
 /**
  * TODO Sprint add-controllers.
