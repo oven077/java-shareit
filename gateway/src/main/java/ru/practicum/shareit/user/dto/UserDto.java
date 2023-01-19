c560b7140cea109060adb2b2216f4d0fa5ea2f37
Andres <oven077@gmail.com>
2023-01-19 13:01:15 +0300

    both services has done (doesnt work controller)

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,4 +1,23 @@
 package ru.practicum.shareit.user.dto;
 
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Builder
+@Data
 public class UserDto {
+    private int id;
+    @NotNull
+    @NotBlank(message = "Invalid name")
+    private String name;
+    @NotNull
+    @Email
+    @Schema(type = "string", example = "test@test.com")
+    private String email;
 }
